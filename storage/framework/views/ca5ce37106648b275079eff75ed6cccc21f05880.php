10cca68b0bfe6414bb699409f6ac8f365b81d405
Mettawijaya <mettawijayawu@gmail.com>
2026-09-08 09:42:19 +0700

    Delete storage directory

--- storage/framework/views/ca5ce37106648b275079eff75ed6cccc21f05880.php
+++
@@ -1,3 +0,0 @@
-<img src="<?php echo e(asset('img/logo.png')); ?>"
-    alt="Logo PPDB Sekolah"
-    <?php echo e($attributes->merge(['style' => 'width: 50px; height: auto;'])); ?>><?php /**PATH C:\xampp\htdocs\lr_simdik\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
